9939a6ddb72f462b58db896e58360ee08d088c2e
Rajesh2105280070 <rajesh.pattanaik2000@gmail.com>
2025-08-09 13:20:44 +0530

    Multiple Image Post and fetched, UI Developed and also Full Screen ad and the card ad

--- src/constants/versionControll.js
+++ src/constants/versionControll.js
@@ -1,3 +1,3 @@
 export const AndroidVersion="1.0.2"; // Current app version - API returns 1.0.1
-export const IOSVersion="1.0.0";
+export const IOSVersion="1.0.1";
 export const ForceUpdate = true; // Enable force update for testing
